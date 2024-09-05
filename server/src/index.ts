0f9b5c40504c53415b957b3178e04c57caf4a43c
cuong <lehuucuong.vnua@gmail.com>
2024-09-05 14:16:33 +0700

    setting server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,23 @@
-console.log("hello world him");
+require("dotenv").config();
+import "reflect-metadata";
+import express from "express";
+import { createConnection } from "typeorm";
+import { User } from "./entities/User";
+
+const main = async () => {
+  await createConnection({
+    type: "postgres",
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: "reddit",
+    logging: true,
+    synchronize: true,
+    entities: [User],
+  });
+
+  const app = express();
+
+  app.listen(4000, () => console.log("server listening on port 4000"));
+};
+
+main().catch((err) => console.log(err));
